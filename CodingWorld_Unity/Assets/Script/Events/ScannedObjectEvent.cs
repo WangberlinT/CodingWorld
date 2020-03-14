7b1f54d24137ad0aac0d8d1558e2cae00700a23e
WangberlinT <11711613@mail.sustech.edu.cn>
2020-03-14 18:36:19 +0800

    修复

---
+++ CodingWorld_Unity/Assets/Script/Events/ScannedObjectEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Events
+{
+    public class ScannedObjectEvent
+    {
+        //todo 整个的逻辑和内容都需要设计
+        private Vector3 myPos;
+        private Collider collider;
+
+        public ScannedObjectEvent()
+        {
+        }
+
+
+    }
+}
+
--- CodingWorld_Unity/Assets/Script/Sight/BasicSight.cs
+++ CodingWorld_Unity/Assets/Script/Sight/BasicSight.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BasicSight : MonoBehaviour
+using CodeWorldInterface;
+using Events;
+public class BasicSight : MonoBehaviour, SightSubject
 {
     public float view_radius = 8.0f;
     public float ray_num = 30;
     public float sight_angle = 90;
+
+    private List<SightObserver> observers;
     private void Update()
     {
         DrawFiledOfView();
@@ -14,6 +17,7 @@ public class BasicSight : MonoBehaviour
 
     private void DrawFiledOfView()
     {
+        List<GameObject> on_sight_objs;
         float angle_step = sight_angle / ray_num;
         Vector3 sight_left = Quaternion.Euler(0, -sight_angle/2, 0) * transform.forward * view_radius;
         //共有ray_num条射线
@@ -30,10 +34,27 @@ public class BasicSight : MonoBehaviour
             if(hitt.transform != null)//hitt是什么
             {
                 end_pos = hitt.point;
+
+                Debug.Log("find: "+hitt.collider.name);
             }
 
             Debug.DrawLine(transform.position, end_pos, Color.red);
         }
         
+    }
+
+    public void AddObserver(SightObserver s)
+    {
+        observers.Add(s);
+    }
+
+    public void NotifyObservers()
+    {
+        //todo 还没有写相关逻辑
+        ScannedObjectEvent s = new ScannedObjectEvent();
+        for(int i = 0;i < observers.Count;i ++)
+        {
+            observers[i].OnScannedObject(s);
+        }
     }
 }
