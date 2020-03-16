b6e0ace4f6d0ced761645124958727af92d8c44f
Hamojpk <49470292+Hamojpk@users.noreply.github.com>
2020-03-16 16:20:39 +0800

    More Position Calulate

--- CodingWorld_Unity/Assets/Script/movement/Movable.cs
+++ CodingWorld_Unity/Assets/Script/movement/Movable.cs
@@ -16,12 +16,12 @@ public class Movable : BasicMovement
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            jump();
+            MoveTo(withAngle(45, 4), 4);
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
            head(2,0.5f);
-            left(2, 0.5f);
+
 
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -56,62 +56,121 @@ public class Movable : BasicMovement
 
     public Vector3 getHeading()
     {
-        return rb.transform.forward;
+        return transform.forward;
     }
 
     public override void head(float distance, float maxspeed)
     {
-        Vector3 destn = rb.transform.forward.normalized * distance;
-        MoveToPosition(destn,distance,maxspeed);
+        
+        Vector3 destn = transform.forward.normalized * distance;
+        Debug.Log(rb.position + destn);
+        MoveTo(rb.position + destn, distance,maxspeed);
 
     }
 
-    private void MoveToPosition(Vector3 destn,float distance, float maxspeed)
+    private void MoveTo(Vector3 destn,float distance, float speed)
     {
        
-        iTween.MoveAdd(gameObject, destn, distance/maxspeed);
+        iTween.MoveTo(gameObject, destn, distance/speed);
     }
 
     public override void jump()
     {
-        rb.AddForce(rb.transform.up * 200);
+        rb.AddForce(transform.up * 200);
     }
 
     public override void left(float distance, float maxspeed)
     {
        
-        Vector3 destn =  -1*rb.transform.right.normalized * distance;
+        Vector3 destn =  -1*transform.right.normalized * distance;
        
-        MoveToPosition(destn, distance, maxspeed);
+        MoveTo(rb.position + destn, distance, maxspeed);
 
 
     }
 
     public override void right(float distance, float maxspeed)
     {
-        Vector3 destn = rb.transform.right.normalized * distance;
+        Vector3 destn = transform.right.normalized * distance;
        
-        MoveToPosition(destn, distance, maxspeed);
+        MoveTo(rb.position + destn, distance, maxspeed);
 
     }
 
     public override void tail(float distance, float maxspeed)
     {
-        Vector3 destn = -1* rb.transform.forward.normalized * distance;
+        Vector3 destn = -1* transform.forward.normalized * distance;
         
-        MoveToPosition(destn, distance, maxspeed);
+        MoveTo(rb.position + destn, distance, maxspeed);
 
     }
 
     public override void turnLeft(float degree)
     {
-
-        iTween.RotateAdd(gameObject, new Vector3(0,-1* degree, 0), 1);
+        iTween.RotateAdd(gameObject, new Vector3(0, -1*degree, 0), 1);
 
     }
     
     public override void turnRight(float degree)
     {
         iTween.RotateAdd(gameObject, new Vector3(0, degree, 0), 1);
+    }
+
+    public Vector3 Forward(float distance)
+    {
+        return Forward(rb.position, distance);
+    }
+
+    public Vector3 Forward(Vector3 startposition, float distance)
+    {
+        return startposition + transform.forward.normalized * distance;
+    }
+
+
+    public Vector3 Left(float distance)
+    {
+        return Left(rb.position, distance);
+    }
+
+    public Vector3 Left(Vector3 startposition, float distance)
+    {
+        return startposition + transform.right.normalized * distance*-1;
+    }
+
+    public Vector3 Right(float distance)
+    {
+        return Right(rb.position, distance);
+    }
+
+    public Vector3 Right(Vector3 startposition, float distance)
+    {
+        return startposition + transform.right.normalized * distance;
+    }
+    public Vector3 Behind(float distance)
+    {
+        return Behind(rb.position, distance);
+    }
+
+    public Vector3 Behind(Vector3 startposition, float distance)
+    {
+        return startposition + transform.forward.normalized * distance*-1;
+    }
+    
+    public void MoveTo(Vector3 position, float speed)
+    {
+        float dis = Vector3.Distance(rb.position, position);
+        iTween.MoveTo(gameObject, position,dis/speed);
+    }
+
+    public Vector3 withAngle(float angle, float distance)
+    {
+        return withAngle(rb.position, angle, distance);
+    }
+
+    public Vector3 withAngle(Vector3 startposition, float angle, float distance)
+    {
+        Quaternion rotation = Quaternion.AngleAxis(angle, transform.up); ;
+        Vector3 dir = rotation * transform.forward;
+        return startposition + dir.normalized * distance;
     }
 }
