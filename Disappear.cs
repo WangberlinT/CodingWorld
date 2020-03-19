2fbea2de3da6aee67ef13e068e28a722596c8eee
WangberlinT <11711613@mail.sustech.edu.cn>
2020-03-19 20:41:29 +0800

    清理

--- Disappear.cs
+++
@@ -1,19 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Disappear : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {    
-    }
-    //
-    void Update()
-    {             if (Input.GetKeyUp(KeyCode.A))
-        {
-            gameObject.SetActive(false);
-        }
-    }
-}
-
