e7c1b17c21b1d61c7d2fd1a035061e0305580d83
Hamojpk <49470292+Hamojpk@users.noreply.github.com>
2020-04-19 20:41:41 +0800

    a little fix

---
+++ CompilerApp/ConsoleApp2/ConsoleApp2/ServerSend.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompilerServer
+{
+    /*
+     * ServerSend 包含了从Server端向Client端发送信息的一些方法
+     * SendTCPData: 以Packet形式发送
+     * SendTCPDataToAll: 给所有Client 发送
+     * Welcome: 向Client 发送字符串
+     */
+    class ServerSend
+    {
+        private static void SendTCPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Server.clients[_toClient].tcp.SendData(_packet);
+        }
+
+        private static void SendTCPDataToAll(Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.maxConnection; i++)
+            {
+                Server.clients[i].tcp.SendData(_packet);
+            }
+        }
+        private static void SendTCPDataToAll(int _exceptClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.maxConnection; i++)
+            {
+                if (i != _exceptClient)
+                {
+                    Server.clients[i].tcp.SendData(_packet);
+                }
+            }
+        }
+
+        public static void Welcome(int _toClient, string _msg)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.welcome))
+            {
+                _packet.Write(_msg);
+                _packet.Write(_toClient);
+
+                SendTCPData(_toClient, _packet);
+            }
+        }
+    }
+}
